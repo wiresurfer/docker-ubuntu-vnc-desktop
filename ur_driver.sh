fad2f32c18b04a4c5565672c9e78d53f87cf38f4
shaishavkr <shaishav.kumar@rapyuta-robotics.com>
2020-09-09 03:15:09 +0530

    Added UR Driver config

---
+++ ur_driver.sh
@@ -0,0 +1,10 @@
+source /opt/ros/kinetic/setup.bash
+mkdir -p catkin_ws/src && cd catkin_ws
+
+git clone https://github.com/UniversalRobots/Universal_Robots_ROS_Driver.git src/Universal_Robots_ROS_Driver
+git clone -b calibration_devel https://github.com/fmauch/universal_robot.git src/fmauch_universal_robot
+
+apt update && apt-get upgrade
+rosdep update
+rosdep install --from-paths src --ignore-src -y
+catkin build
